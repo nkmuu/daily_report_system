135100c46cf303ae9ac4adf3d398f941596973b7
muu <mmmmmuuuuu0105@gmail.com>
2021-01-11 01:21:21 +0900

    Add Follow class

---
+++ src/models/Follow.java
@@ -0,0 +1,69 @@
+package models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@Table(name = "follows")
+@NamedQueries({
+    @NamedQuery(
+            name = "getAllFollows",
+            query = "SELECT f FROM Follow AS f ORDER BY f.id DESC"
+            ),
+    @NamedQuery(
+            name = "getFollowsCount",
+            query = "SELECT COUNT(f) FROM Follow AS f"
+            )
+
+})
+
+@Entity
+public class Follow {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "employee_id", nullable = false)
+    private Employee employee;
+
+    @ManyToOne
+    @JoinColumn(name = "follower_id", nullable = false)
+    private Employee follower;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public Employee getFollower() {
+        return follower;
+    }
+
+    public void setFollower(Employee follower) {
+        this.follower = follower;
+    }
+
+
+
+}
---
+++ src/models/validators/FollowValidator.java
@@ -0,0 +1,17 @@
+package models.validators;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import models.Follow;
+
+public class FollowValidator {
+    public static List<String> validate(Follow f) {
+        List<String> errors = new ArrayList<String>();
+
+        return errors;
+
+    }
+
+}
+
